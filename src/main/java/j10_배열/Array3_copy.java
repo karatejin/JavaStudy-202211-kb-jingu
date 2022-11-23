eb5a8a02843626a13107de0452b731f961881783
karatejin <junsyang@naver.com>
2022-11-23 13:47:12 +0900

    홍누나 코드

--- src/main/java/j10_배열/Array3_copy.java
+++ src/main/java/j10_배열/Array3_copy.java
@@ -16,24 +16,26 @@ public class Array3_copy {
 
         /*배열에 랜덤한 값 대입*/
 
-        for (int i = 0; i < randomArray.length; i++) {
-            while (true) { /**/
+        for (int i = 0; i < randomArray.length; i++) { /*배열 길이만큼 반복*/
+            while (true) { /*무한반복*/
 
-                int randomNumber = random.nextInt(10) + 1;
-                int count = 0;
+                int randomNumber = random.nextInt(10) + 1; /*랜덤숫자 생성*/
+                int count = 0; /*배열 대입 여부 boolean 생성*/
 
-                for (int j = 0; j < randomArray.length; j++) { /**/
-                    if (randomArray[j] == randomNumber){
-                        count++;
+                for (int j = 0; j < randomArray.length; j++) { /*배열 요소 전부 검수*/
+                    if (randomArray[j] == randomNumber){ /*배열요소에 랜덤 숫자와 동일한 요소가 있을경우*/
+                        count++; /*배열에 대입되지 않고*/
+                        System.out.println("중복 인덱스 : " + i + "/중복 숫자 : " + randomArray[i]);
                         break;
                     }
                 }
+                /*found == ture*/
                 if (count == 0){
-                    randomArray[i] = randomNumber;
-                    break; /*while 의 break*/
+                    randomArray[i] = randomNumber; /*i 번째 배열에 랜덤숫자가 대입됨*/
+                    System.out.println(randomArray[i] + " - 대입성공");
+                    break; /*while문 탈출 -->  for문 반복*/
                     }
             }
-
         }
 
         /*배열에서 값을 하나씩 꺼내어 total 에 더한다.
@@ -46,7 +48,6 @@ public class Array3_copy {
             if(i != randomArray.length-1){
                 System.out.print(", ");
         }
-
     }
       System.out.println();
       System.out.println("총합 : " + total);
